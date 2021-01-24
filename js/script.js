aec7c16b40cb27b6eaf95df639dc315b13304b4a
Dovtutis <dovtutis@gmail.com>
2021-01-24 13:30:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// NAVBAR FOR MOBILE
+
+const navbar = document.getElementById("navbar");
+const navbarToggle = navbar.querySelector(".navbar-toggle");
+const navbarMenu = navbar.querySelector(".navbar-menu");
+const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+navbarToggle.addEventListener("click", () => {
+    if (navbar.classList.contains("opened")) {
+        closeMobileNavbar();
+    } else {
+        openMobileNavbar();
+    }
+});
+
+navbarLinksContainer.addEventListener("click", (clickEvent) => {
+    clickEvent.stopPropagation();
+});
+
+navbarMenu.addEventListener("click", closeMobileNavbar);
+
+function openMobileNavbar() {
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+}
+
+function closeMobileNavbar() {
+    navbar.classList.remove("opened");
+    navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+}
+
+
