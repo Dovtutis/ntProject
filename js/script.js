38f07f40abd126c5a2b4029c4bb9b2620984f6d8
Dovtutis <dovtutis@gmail.com>
2021-02-13 13:26:37 +0200

    image fetch from unsplash API working, image card creation function done, added gitignore file

--- js/script.js
+++ js/script.js
@@ -34,10 +34,69 @@ function closeMobileNavbar() {
 const scrollLeftButton = document.getElementById('scroll-left-button');
 const scrollRightButton = document.getElementById('scroll-right-button');
 const galleryContainer = document.querySelector('.sliding-container');
+const requestUrl = 'https://api.unsplash.com/search/collections?page=1&per_page=6&query=home interior&client_id=IW3u5fbyKrQ1PDWZn5ZLo56AoiSLuOBxR6Fjb76YkCI';
+const imagesArray = [];
+let coordinate = 0;
+
+//EVENT LISTENERS
+
 scrollLeftButton.addEventListener('click', scrollLeft);
 scrollRightButton.addEventListener('click', scrollRight);
 
-let coordinate = 0;
+
+getImages();
+
+async function getImages () {
+    fetch(requestUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            data.results.forEach((image) => {
+                imagesArray.push(image.preview_photos[0].urls.regular)
+            });
+            createGalleryCards(imagesArray);
+        }).catch(error => console.error())
+}
+
+function createGalleryCards (imagesArray){
+    imagesArray.forEach((image) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let img = document.createElement("img");
+        img.setAttribute('src', image);
+        img.setAttribute('alt', 'home interior');
+        img.classList.add("card-image");
+        let textContainer = document.createElement("div");
+        textContainer.classList.add("text-container");
+        let headline = document.createElement("h2");
+        headline.classList.add("py-1");
+        let paragraph = document.createElement("p");
+        paragraph.innerText = "Lorem ipsum dolor sit amet in nam, consectetur adipisicing elit. Fugit hic, impedit in nam odio quia?"
+        let arrowIcon = document.createElement("i");
+        arrowIcon.classList.add("fas");
+        arrowIcon.classList.add("fa-arrow-right");
+        arrowIcon.classList.add("pb-2");
+        textContainer.appendChild(headline);
+        textContainer.appendChild(paragraph);
+        textContainer.appendChild(arrowIcon);
+        card.appendChild(img);
+        card.appendChild(textContainer);
+        galleryContainer.appendChild(card);
+    })
+}
+
+// <div className="card">
+//     <img src="https://i.pinimg.com/originals/5b/65/31/5b65311e67cdc2ab9b5b1ba8516878cb.jpg"
+//          alt="" className="card-image">
+//         <div className="text-container">
+//             <h2 className="py-1">Lorem ipsum dolor sit amet</h2>
+//             <p>
+//                 Lorem ipsum dolor sit amet in nam, consectetur adipisicing elit. Fugit hic, impedit
+//                 in nam odio quia?
+//             </p>
+//             <i className="fas fa-arrow-right pb-2"></i>
+//         </div>
+// </div>
 
 function scrollLeft (){
     let cardWidth = document.querySelector('.card').offsetWidth;
